79d6d2882cd5b73d94785de8f5750c06da306837
Mero <1043526080@qq.com>
2021-06-02 14:44:43 +0800

    重写Equals方法的原理代码

--- day01_API/src/main/java/com/study/Demo02Equals.java
+++ day01_API/src/main/java/com/study/Demo02Equals.java
@@ -1,9 +1,11 @@
 package com.study;
 
+import java.util.Random;
+
 public class Demo02Equals {
     public static void main(String[] args) {
         Person mm = new Person("美美", 8);
-        Person jh = new Person("镜华", 9);
+        Person jh = new Person("美美", 8);
         boolean b = mm.equals(jh);
         /*     源码
         *     public boolean equals(Object obj) {
@@ -17,9 +19,25 @@ public class Demo02Equals {
                 *
                }
         * */
+        /*
+        这里值一样的原因是让jh == mm,使他们等于同一个堆内存地址
         jh = mm;
         System.out.println(mm);
         System.out.println(jh);
-        System.out.println(b);          //值为false
+        */
+        /*  这里通过重写equals方法，使他们比较的是值，这样才有可比较性
+        System.out.println(mm);
+        System.out.println(jh);*/
+
+//          这里其实也有一个问题，当传入的参数不是Person的时候，这样就会报错 ClassCastException （类型转换异常），所以equals代码要进行优化一下，加上判断是否一个Person对象
+//        Random random = new Random();
+//        boolean b1 = mm.equals(random);
+
+        boolean b2 = mm.equals(mm);
+
+
+
+
+        System.out.println(b2);
     }
 }
--- day01_API/src/main/java/com/study/Person.java
+++ day01_API/src/main/java/com/study/Person.java
@@ -1,6 +1,8 @@
 package com.study;
 
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private int age;
@@ -28,8 +30,68 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+    /*
+    *   Object的Equals方法默认比较的是地址值，没有意义，所以需要重写equals方法用来比较属性值（name,age）
+    * 问题：
+    *   隐含一个多态：
+    *   Object obj = p2 = new Person("mm",8)
+    *   多态的坏处，无法使用子类的方法。但是可以通过向下转型来解决
+    * */
 
-//    @Override
+    //jdk自带的方法，推荐！！！
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);  //这里用到了Object这个工具类里面的方法
+    }
+
+    /*
+
+    //Idea自带的方法
+    @Override
+    public boolean equals(Object o) {
+//        传入的就是自己，返回true
+        if (this == o) return true;
+        //如果传过来的是空，或者利用反射传过来的不是同一个类型的对象，返回false   getClass 这里用到了反射机制
+        if (o == null || getClass() != o.getClass()) return false;
+        //如果能走到这一步，则将传过来的对象进行强转
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return name.equals(person.name);
+    }
+*/
+
+    /*
+
+    @Override
+    public boolean equals(Object o) {
+        //这里还有2中情况，当传入的参数类型是一个空和本身的情况
+        if(o == null){
+            return false;
+        }
+
+        //传递过来的就是本身
+        if(this == o){          //提高程序效率
+            return true;
+        }
+
+
+        //如果传过来的对象是一个Person类型，则进行强转然后比较，如果不是，则直接返回false
+        if(o instanceof Person){
+            Person p = (Person) o;
+            boolean b = this.name.equals(p.name) && this.age == p.age;
+            return b;
+        }
+        return false;
+
+    }
+
+*/
+    //    @Override
 //    public String toString() {
 ////        return "ccc";
 //        return "name"+name+"age"+age;
