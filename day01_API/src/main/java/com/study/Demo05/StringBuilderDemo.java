4b42aec6e15e5da861a5a31920a6fefb84f6baaa
Mero <1043526080@qq.com>
2021-06-06 15:27:05 +0800

    StringBuilder的链式编程，引用数据类型的比较，以及字符串的大小写转换toUpperCase().toLowerCase()

--- day01_API/src/main/java/com/study/Demo05/StringBuilderDemo.java
+++ day01_API/src/main/java/com/study/Demo05/StringBuilderDemo.java
@@ -10,8 +10,25 @@ package com.study.Demo05;
  */
 public class StringBuilderDemo {
     public static void main(String[] args) {
-        demo01String();
+//        demo01String();
 //        demo02StringBuilder();
+//        demo03StringBuilder();
+        demo04StringUpcaseLowerCase();
+    }
+
+    private static void demo04StringUpcaseLowerCase() {
+        System.out.println("jh".toUpperCase());         //字母的大小写转换
+        System.out.println("mm".toUpperCase().toLowerCase().toUpperCase().toLowerCase());
+    }
+
+    private static void demo03StringBuilder() {
+        StringBuilder stringBuilder = new StringBuilder("a");       //StringBuilder的有参构造
+        System.out.println("stringBuilder="+stringBuilder);
+        StringBuilder b = stringBuilder.append("b");
+        System.out.println(b);
+        System.out.println(stringBuilder==b);   //引用数据类型比较的是堆中的地址值
+        StringBuilder c = b.append("mm").append(1).append(0.3);     //可以使用链式编程，啥都可以添加到这个StringBuilder里面去
+        System.out.println(c);
     }
 
     private static void demo02StringBuilder() {
