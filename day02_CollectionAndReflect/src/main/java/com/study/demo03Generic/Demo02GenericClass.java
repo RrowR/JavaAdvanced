f9fc0dfbc31bbb4401991bcd10899767b1ef80dd
Mero <1043526080@qq.com>
2021-06-07 22:32:28 +0800

    类泛型的定义

--- day02_CollectionAndReflect/src/main/java/com/study/demo03Generic/Demo02GenericClass.java
+++ day02_CollectionAndReflect/src/main/java/com/study/demo03Generic/Demo02GenericClass.java
@@ -2,6 +2,10 @@ package com.study.demo03Generic;
 
 import lombok.Data;
 
+/**
+ * 类泛型的定义，凡是返回值的地方全部使用E,并且在类上也定义一个<E>用来接收传过来的泛型
+ * @param <E>
+ */
 @Data
 public class Demo02GenericClass<E> {
     private E name;
