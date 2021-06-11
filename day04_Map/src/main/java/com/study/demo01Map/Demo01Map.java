71c4e91230781564da01ae1688dec2ea5be16bd6
Mero <1043526080@qq.com>
2021-06-11 17:23:26 +0800

    Map集合的介绍

---
+++ day04_Map/src/main/java/com/study/demo01Map/Demo01Map.java
@@ -0,0 +1,15 @@
+package com.study.demo01Map;
+
+/**
+ * java.util
+ *  Interface Map<K,V>
+ *     1.Map是一个双列集合，和Collection集合完全不一样的单独存在，有2个泛型
+ *     2.K,V的数据类型可以不同
+ *     3.K是不允许重复的，V是可以重复的
+ *     4.Map集合中的元素，Key和Value是一一对应的
+ */
+public class Demo01Map {
+    public static void main(String[] args) {
+
+    }
+}
