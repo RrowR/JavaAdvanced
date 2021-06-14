f28eac68d9bee7719932db2ed3ce1b2976b6a82b
Mero <2415782224@qq.com>
2021-06-14 19:23:43 +0800

    老杜的节日转码小练习

---
+++ PersonalExamination/src/main/java/com/study/DuanWuHappy.java
@@ -0,0 +1,14 @@
+package com.study;
+
+public class DuanWuHappy {
+    public static void main(String[] args) {
+        int 端 = 31471;
+        int 午 = 21320;
+        int 安 = 23433;
+        int 康 = 24247;
+        System.out.println(((char) 端));
+        System.out.println(((char) 午));
+        System.out.println(((char) 安));
+        System.out.println(((char) 康));
+    }
+}
