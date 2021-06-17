3353aa69c7a001b3abdb00ddae26d9de7424c8ef
Mero <2415782224@qq.com>
2021-06-17 11:07:16 +0800

    Thread类的常用方法_获取线程名称的方法 1.通过重写run方法，然后使用start来进行调用 2.通过Thread类里的静态方法，Thread.currentThread()来进行调用

---
+++ day06_Thread/src/main/java/com/study/Demo01Thread.java
@@ -0,0 +1,12 @@
+package com.study;
+
+public class Demo01Thread {
+    public static void main(String[] args) {
+        ThreadClass t = new ThreadClass();
+        ThreadClass t2 = new ThreadClass();
+        t.start();
+        t2.start();
+        Thread thread = Thread.currentThread();
+        System.out.println(thread);
+    }
+}
---
+++ day06_Thread/src/main/java/com/study/ThreadClass.java
@@ -0,0 +1,12 @@
+package com.study;
+
+public class ThreadClass extends Thread{
+    @Override
+    public void run() {
+        //获取线程名称的方式
+//        String name = getName();
+//        System.out.println(name);
+        Thread thread = Thread.currentThread();
+        System.out.println(thread);
+    }
+}
