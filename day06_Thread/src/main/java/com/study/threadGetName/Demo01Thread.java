be98c760957565f2f6cd52ff1d786dbfd543b817
Mero <2415782224@qq.com>
2021-06-17 11:15:25 +0800

    update

--- day06_Thread/src/main/java/com/study/threadGetName/Demo01Thread.java
+++ day06_Thread/src/main/java/com/study/threadGetName/Demo01Thread.java
@@ -1,4 +1,4 @@
-package com.study;
+package com.study.threadGetName;
 
 public class Demo01Thread {
     public static void main(String[] args) {
--- day06_Thread/src/main/java/com/study/threadGetName/ThreadClass.java
+++ day06_Thread/src/main/java/com/study/threadGetName/ThreadClass.java
@@ -1,4 +1,4 @@
-package com.study;
+package com.study.threadGetName;
 
 public class ThreadClass extends Thread{
     @Override
