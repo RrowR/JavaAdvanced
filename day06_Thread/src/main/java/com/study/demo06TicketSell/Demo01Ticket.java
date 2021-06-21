8ce32339a273979a592813c12c1c9f32e35138ba
Mero <2415782224@qq.com>
2021-06-21 12:42:08 +0800

    update

--- day06_Thread/src/main/java/com/study/demo06TicketSell/Demo01Ticket.java
+++ day06_Thread/src/main/java/com/study/demo06TicketSell/Demo01Ticket.java
@@ -8,6 +8,14 @@ public class Demo01Ticket implements Runnable{
         //这里必须要进行循环，不循环程序就只会执行一次
         while (ticket > 0){
             if (ticket > 0){
+                /**
+                 * 此代码的目的是为了提高代码出现问题的几率
+                 */
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println(Thread.currentThread().getName()+"出售第"+ticket+"张票");
                 ticket--;
             }
