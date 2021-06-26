3cf2610d650ec7101df25c801211e8fbd7a9a476
Memo <2415782224@qq.com>
2021-06-26 10:46:56 +0800

    lambda表达式的总结

--- day07_ThreadAndLambda/src/main/java/com/study/demo03Lambda/Demo01LambdaExpression.java
+++ day07_ThreadAndLambda/src/main/java/com/study/demo03Lambda/Demo01LambdaExpression.java
@@ -13,11 +13,18 @@ public class Demo01LambdaExpression {
         }).start();
 
         /*
-        * Lambda表达式的使用：
-        *       1. （） 代表Thread里的无参run方法
-        *       2.  ->  代表要实现的方法体
-        *       3.  {具体实现方法的内容}
-        * */
+    Lambda表达式的标准格式:
+        由三部分组成:
+            a.一些参数
+            b.一个箭头
+            c.一段代码
+        格式:
+            (参数列表) -> {一些重写方法的代码};
+        解释说明格式:
+            ():接口中抽象方法的参数列表,没有参数,就空着;有参数就写出参数,多个参数使用逗号分隔
+            ->:传递的意思,把参数传递给方法体{}
+            {}:重写接口的抽象方法的方法体
+ */
         new Thread(() -> {System.out.println(Thread.currentThread().getName() + "线程执行了third");}).start();
         
     }
