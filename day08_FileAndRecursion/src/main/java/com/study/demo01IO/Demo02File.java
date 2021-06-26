b8202dd8f39078626090a5113091dcc20e7d6445
Memo <2415782224@qq.com>
2021-06-27 01:30:20 +0800

    这里就提一下相对路径，是相对于根目录下的路径 windows上的\\是转义字符，相当于1个\

---
+++ day08_FileAndRecursion/src/main/java/com/study/demo01IO/Demo02File.java
@@ -0,0 +1,12 @@
+package com.study.demo01IO;
+
+/**
+ * 绝对路径：不提
+ * 相对路径：相对于我这个maven的root下的路径 C:\Users\Atlantis\Documents\Ideaspace\JavaAdvanced
+ * 此时的路径就是在这个路径的基础之上  ...\a.txt
+ * 注意：
+ *      1.路径不区分大小写
+ *      2.Windows上使用的是\反斜杠，\ 是一个转义字符，\\代表一个普通的\
+ */
+public class Demo02File {
+}
