9a410a48ac3cff81f1fa80019597c2a34d32b1f7
Memo <2415782224@qq.com>
2021-06-26 23:40:50 +0800

    java.io包里的file类介绍

---
+++ day08_FileAndRecursion/src/main/java/com/study/demo01IO/Demo01IO.java
@@ -0,0 +1,28 @@
+package com.study.demo01IO;
+
+/**
+ * java.lang.Object
+ *   继承者 java.io.File
+ *    所有已实现的接口：
+ *    Serializable, Comparable<File>
+ *  文件和目录路径名的抽象表示形式。
+ *     java把电脑中的文件和文件夹(目录)封装为了一个File类,我们可以使用File类对文件和文件夹进行操作
+ *     我们可以使用File类的方法
+ *         创建一个文件/文件夹
+ *         删除文件/文件夹
+ *         获取文件/文件夹
+ *         判断文件/文件夹是否存在
+ *         对文件夹进行遍历
+ *         获取文件的大小
+ *     File类是一个与系统无关的类,任何的操作系统都可以使用这个类中的方法
+ *
+ *     重点:记住这三个单词
+ *         file:文件
+ *         directory:文件夹/目录
+ *         path:路径
+ */
+public class Demo01IO {
+    public static void main(String[] args) {
+
+    }
+}
