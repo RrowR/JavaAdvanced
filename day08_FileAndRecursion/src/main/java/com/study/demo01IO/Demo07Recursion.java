85f003250ba637cedd9611d01d85b84c15e1acc9
Memo <2415782224@qq.com>
2021-06-29 23:01:50 +0800

    递归的注意事项

---
+++ day08_FileAndRecursion/src/main/java/com/study/demo01IO/Demo07Recursion.java
@@ -0,0 +1,37 @@
+package com.study.demo01IO;
+
+/**
+ * 递归：自己调用自己
+ *      注意：
+ *          1.递归一定要终止自己的方法
+ *          2.递归次数不能太多，否则会造成栈溢出     Exception in thread "main" java.lang.StackOverflowError
+ *      a() 方法会一直调用a方法，在栈内存中越来越多，导致栈内存溢出
+ *      构造方法禁止递归
+ */
+public class Demo07Recursion {
+    public static void main(String[] args) {
+//        a();
+        b(1);       // 11250 的时候，程序出现异常
+    }
+
+    /**
+     * 编译报错，禁止递归
+     */
+    public Demo07Recursion() {
+//        Demo07Recursion();
+    }
+
+    private static void b(int i) {
+        System.out.println(i);
+        if (i == 20000){
+            return;
+        }
+        b(++i);     // 这里不能使用 i++ 不然传入的值一直是1
+    }
+
+    public static void a() {
+        System.out.println("自己调用自己");
+        a();
+    }
+}
+
