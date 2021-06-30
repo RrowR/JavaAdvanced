8c070b293d9478d1d739ef2dd2b174feb401f9c6
Memo <2415782224@qq.com>
2021-06-30 09:24:03 +0800

    使用递归来计算阶乘

---
+++ day08_FileAndRecursion/src/main/java/com/study/demo01IO/Demo09Factorial.java
@@ -0,0 +1,18 @@
+package com.study.demo01IO;
+
+/**
+ * 使用递归实现阶乘
+ */
+public class Demo09Factorial {
+    public static void main(String[] args) {
+        int factorial = factorial(4);
+        System.out.println(factorial);
+    }
+
+    public static int factorial(int n){
+        if (n == 1){
+            return 1;       // 结束条件：不再自己调用自己
+        }
+        return n * factorial(n - 1);    // 这里的返回值必须是调用自己的方法才行
+    }
+}
