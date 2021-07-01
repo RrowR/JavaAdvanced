cd50591494e6e4d500e678924d5598bd9f5994f2
Memo <2415782224@qq.com>
2021-07-01 19:20:55 +0800

    使用FileOutputStream里的构造方法，可以开启是否续写 换行符号：windows：\r\n          linux: /n 注意：一定都要要是字节哦

---
+++ day09_IOAndProperties/src/main/java/com/study/demo01/outputstream/Demo03OutputStream.java
@@ -0,0 +1,28 @@
+package com.study.demo01.outputstream;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 文件的追加写和换行
+ */
+public class Demo03OutputStream {
+    public static void main(String[] args) throws IOException {
+        /**
+         *  FileOutputStream(String name, boolean append) 创建一个向具有指定 name 的文件中写入数据的输出文件流。
+         *  FileOutputStream(File file, boolean append) 创建一个向指定 File 对象表示的文件中写入数据的文件输出流。
+         *  换行符号:
+         *      windows: \r\n
+         *      linux:   /n
+         *      mac:    /r
+         */
+//        FileOutputStream fos = new FileOutputStream("day09_IOAndProperties\\c.txt",true);
+        FileOutputStream fos = new FileOutputStream(new File("day09_IOAndProperties\\c.txt"),false);
+        for (int i = 0; i < 10; i++) {
+            fos.write("您好".getBytes());
+            fos.write("\r\n".getBytes());
+        }
+        fos.close();
+    }
+}
