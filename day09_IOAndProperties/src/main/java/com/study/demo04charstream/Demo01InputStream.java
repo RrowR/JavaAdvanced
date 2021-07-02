a6e4a2d34324d13909235901730295a9e38d4a7c
Memo <2415782224@qq.com>
2021-07-02 14:06:33 +0800

    使用字节流读取中文时会出现乱码

--- day09_IOAndProperties/src/main/java/com/study/demo04charstream/Demo01InputStream.java
+++ day09_IOAndProperties/src/main/java/com/study/demo04charstream/Demo01InputStream.java
@@ -1,7 +1,19 @@
 package com.study.demo04charstream;
 
-public class Demo01InputStream {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
 
+/**
+ *  UTF-8 : 1个中文等于3个字符
+ *  GBK:    1个中文等于2个字符
+ */
+public class Demo01InputStream {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("day09_IOAndProperties/c.txt");
+        int len = 0;
+        while ((len = fis.read()) != -1){
+            System.out.print((char) len);         // 将每次读取的1个二进制转10进制的数转换成char类型就会出现乱码
+        }
+        fis.close();
     }
 }
