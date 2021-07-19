6df47a8886caea1b8683931c328899fc65d9dd12
Memo <2415782224@qq.com>
2021-07-19 09:52:54 +0800

    序列化一个数组

---
+++ day10_IO/src/main/java/com/study/demo03Serilizable/Demo03Serializable.java
@@ -0,0 +1,23 @@
+package com.study.demo03Serilizable;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Demo03Serializable {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("day10_IO/serializable.txt"));
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("day10_IO/serializable.txt"));
+        ArrayList<Person> arrayList = new ArrayList<>();
+        arrayList.add(new Person("张三", 22));
+        arrayList.add(new Person("李四",23));
+        arrayList.add(new Person("王五",24));
+        oos.writeObject(arrayList);
+        Object o = ois.readObject();
+        ArrayList<Person> people = (ArrayList<Person>) o;
+        for (Person person : people) {
+            System.out.println(person);
+        }
+        oos.close();
+        ois.close();
+    }
+}
