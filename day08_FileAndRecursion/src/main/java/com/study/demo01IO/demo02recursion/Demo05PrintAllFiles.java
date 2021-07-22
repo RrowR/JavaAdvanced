8c487200c3f6f8852287207ddd6be883c8d53129
Memo <2415782224@qq.com>
2021-07-22 20:46:14 +0800

    update

--- day08_FileAndRecursion/src/main/java/com/study/demo01IO/demo02recursion/Demo05PrintAllFiles.java
+++ day08_FileAndRecursion/src/main/java/com/study/demo01IO/demo02recursion/Demo05PrintAllFiles.java
@@ -7,7 +7,7 @@ import java.io.File;
  */
 public class Demo05PrintAllFiles {
     public static void main(String[] args) {
-        File file = new File("F:\\abc");
+        File file = new File("day08_FileAndRecursion");
         getAllFiles(file);
     }
 
