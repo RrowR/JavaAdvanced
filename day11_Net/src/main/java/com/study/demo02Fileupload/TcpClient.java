3019e306936d8f2d8fcd7a0c69263199b18c3785
Memo <2415782224@qq.com>
2021-08-02 19:50:53 +0800

    重新温习一遍socket(未完成)

--- day11_Net/src/main/java/com/study/demo02Fileupload/TcpClient.java
+++ day11_Net/src/main/java/com/study/demo02Fileupload/TcpClient.java
@@ -1,46 +1,11 @@
 package com.study.demo02Fileupload;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpClient {
-    public static void main(String[] args) throws IOException {
-        // 定义一个自己的输入流来读取要上传的文件
-        BufferedInputStream bis = new BufferedInputStream(new FileInputStream("day11_Net/nekohashuzuku.png"));
-        Socket socket = new Socket("127.0.0.1", 8888);
-        /*
-            获取 socket 里的 输入 输出流
-         */
-        OutputStream socketOutputStream = socket.getOutputStream();
-        InputStream socketInputStream = socket.getInputStream();
-
-        byte[] bytes = new byte[1024];
-        int len;
-        // 将服务器发送指定文件的字节
-        while ((len = bis.read(bytes)) != -1) {
-            socketOutputStream.write(bytes, 0, len);
-        }
-        // 最后一定要关闭socket的输出，否则会进入阻塞状态，源码解析如下
-        // 这个就是在写出的时候打一个结束标记
-        socket.shutdownOutput();
-
-        // 传输完毕的提醒
-//        socketOutputStream.write("文件传入完毕...".getBytes());
-
-        // 读取服务器发送过来的数据
-            while ((len = bis.read(bytes)) != -1) {
-            socketInputStream.read(bytes,0,len);
-            System.out.println(new String(bytes));
-        }
-
-        /*
-            从此输入流中将最多 b.length 个字节的数据读入一个 byte 数组中。在某些输入可用之前，此方法将阻塞。
-         */
-//        socket.shutdownInput();
-
-        // 正常关闭流和socket
-        bis.close();
-        socket.close();
+    public static void main(String[] args){
 
     }
 }
