9be15907944f55d6e594390c25541276aa797773
MeroMemo <1043526080@qq.com>
2021-08-04 20:35:54 +0800

    Client和Server端的代码实现

--- day11_Net/src/main/java/com/study/demo02Fileupload/TcpClient.java
+++ day11_Net/src/main/java/com/study/demo02Fileupload/TcpClient.java
@@ -1,11 +1,40 @@
 package com.study.demo02Fileupload;
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpClient {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        // 创建Client的socket，设置ip(本地)，程序的port用来传输数据
+        Socket socket = new Socket("127.0.0.1", 8888);
+        /*
+            获取client客户端socket的inputstream和outputstream
+         */
+        OutputStream socketOutputStream = socket.getOutputStream();
+        InputStream socketInputStream = socket.getInputStream();
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream("day11_Net/nekohashuzuku.png"));
+        int len;
+        byte[] bytes = new byte[1024];
+        while ((len = bis.read(bytes)) != -1) {
+            /*
+                将本地的BufferedInputStream里读取的图片文件通过socket来进行写出
+                注意：网络传输的过程中一定要是字节才可以
+             */
+            socketOutputStream.write(bytes,0,len);
+        }
+        socket.shutdownOutput();
+        /*
+            向服务端发送一个结束符号，告诉服务端，文件已经传输完毕，否则服务端的serverSocket将进入阻塞状态
+         */
+        while ((len = socketInputStream.read(bytes)) != -1) {
+            // 这里一定要设置起始值和结束值，不然打印出来是一个乱码，因为bytes后面的内容和输出的无关
+            System.out.println(new String(bytes,0,len));
+        }
+
+        socket.close();
+
+
+
 
     }
 }
--- day11_Net/src/main/java/com/study/demo02Fileupload/TcpServer.java
+++ day11_Net/src/main/java/com/study/demo02Fileupload/TcpServer.java
@@ -5,7 +5,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpServer {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        // ServerSocket为服务器端用来接收数据的会话，要设置接收的端口号
+        ServerSocket serverSocket = new ServerSocket(8888);
+        // 服务端需要调用serverSocket.accept方法监听才能获得真正具有文件传输的Socket
+        Socket socket = serverSocket.accept();
+        // 获取socket的输入流，读取流中的内容
+        InputStream socketInputStream = socket.getInputStream();
+        // 获取socket的输入流，向Clinet输出数据
+        OutputStream socketOutputStream = socket.getOutputStream();
+        // 本地创建一个output来写出数据
+        File file = new File("day11_Net/上传的文件夹");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(file, "picture.png")));
+        int len;
+        byte[] bytes = new byte[1024];
+        while ((len = socketInputStream.read(bytes)) != -1) {
+            bos.write(bytes, 0, len);
+        }
+        bos.close();
+        socketOutputStream.write("传输完成!!!".getBytes());
+        // 这里不设置结束标记也不会出现问题，因为下面还是会关闭serverSocket
+//        socket.shutdownOutput();
+        serverSocket.close();
     }
 }
